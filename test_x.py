3732ee1e6f56d1b84c57e767fecfbf02875ec4d2
Nate Gay <nate.gay@wpengine.com>
2018-04-29 22:51:19 -0500

    Working Minimax?

--- test_x.py
+++ test_x.py
@@ -3,20 +3,20 @@ import x
 
 
 class TestMe(unittest.TestCase):
-    def test_who_won(self):
-        self.assertEqual(x.find_winner([[None, None, None]]), None)
-        self.assertEqual(x.find_winner([['x', None, 'x']]), None)
-        self.assertEqual(x.find_winner([['y', 'y', 'y']]), None)
-        self.assertEqual(x.find_winner([['x', None, 'x', 'x', 'x']]), None)
-        self.assertEqual(x.find_winner([['x', 'x']]), None)
-        self.assertEqual(x.find_winner([['x', 'x', 'o']]), None)
-        self.assertEqual(x.find_winner([['x', 'x', 'x']]), 'x')
-        self.assertEqual(x.find_winner([['o', 'o', 'o']]), 'o')
-        self.assertEqual(x.find_winner([['x', None, 'x', 'x']]), 'x')
-        self.assertEqual(x.find_winner([['x', 'x', 'x'], ['x', 'x', 'o']]), 'x')
-        self.assertEqual(x.find_winner([[None, None, None], ['x', 'x', 'x']]), 'x')
+    def test_find_winner(self):
+        self.assertEqual(None, x.find_winner([[None, None, None]]))
+        self.assertEqual(None, x.find_winner([['x', None, 'x']]))
+        self.assertEqual(None, x.find_winner([['y', 'y', 'y']]))
+        self.assertEqual(None, x.find_winner([['x', None, 'x', 'x', 'x']]))
+        self.assertEqual(None, x.find_winner([['x', 'x']]))
+        self.assertEqual(None, x.find_winner([['x', 'x', 'o']]))
+        self.assertEqual('x', x.find_winner([['x', 'x', 'x']]))
+        self.assertEqual('o', x.find_winner([['o', 'o', 'o']]))
+        self.assertEqual('x', x.find_winner([['x', None, 'x', 'x']]))
+        self.assertEqual('x', x.find_winner([['x', 'x', 'x'], ['x', 'x', 'o']]))
+        self.assertEqual('x', x.find_winner([[None, None, None], ['x', 'x', 'x']]))
 
-    def test_possible_wins(self):
+    def test_collate_possible_win_groups(self):
         input_board = [
             ['1', '2', '3'],
             ['4', '5', '6'],
@@ -34,20 +34,14 @@ class TestMe(unittest.TestCase):
         ]
         self.assertEqual(expect, x.collate_possible_win_groups(input_board))
 
-    def test_who_goes_next(self):
-        self.assertEqual('x', x.who_goes_next(None))
-        self.assertEqual('x', x.who_goes_next('o'))
-        self.assertEqual('o', x.who_goes_next('x'))
-        self.assertEqual('x', x.who_goes_next(123))
+    def test_get_current_turn(self):
+        self.assertEqual('x', x.get_current_turn([[None, None, None], [None, None, None], [None, None, None]]))
+        self.assertEqual('o', x.get_current_turn([['x', None, None], [None, None, None], [None, None, None]]))
+        self.assertEqual('x', x.get_current_turn([['x', 'o', None], [None, None, None], [None, None, None]]))
+        self.assertEqual('x', x.get_current_turn([['x', 'x', None], [None, None, None], [None, None, None]]))
+        self.assertEqual(None, x.get_current_turn([['x', 'x', 'x']]))
 
-    def test_who_goes_next2(self):
-        self.assertEqual('x', x.who_goes_next2([[None, None, None], [None, None, None], [None, None, None]]))
-        self.assertEqual('o', x.who_goes_next2([['x', None, None], [None, None, None], [None, None, None]]))
-        self.assertEqual('x', x.who_goes_next2([['x', 'o', None], [None, None, None], [None, None, None]]))
-        self.assertEqual('x', x.who_goes_next2([['x', 'x', None], [None, None, None], [None, None, None]]))
-        self.assertEqual(None, x.who_goes_next2([['x', 'x', 'x']]))
-
-    def test_find_best_moves(self):
+    def test_maximize_moves(self):
         available_moves = [
             x.Move(row=0, col=1, score=-16),
             x.Move(row=1, col=2, score=6),
@@ -55,24 +49,46 @@ class TestMe(unittest.TestCase):
             x.Move(row=2, col=2, score=1),
             x.Move(row=2, col=2, score=6),
         ]
-        expected_best_moves = x.BestMoves(
+        expected_sorted_moves = x.SortedMoves(
             score=6,
             moves=[
                 x.Move(row=1, col=2, score=6),
                 x.Move(row=2, col=2, score=6),
             ]
         )
-        self.assertEqual(expected_best_moves, x.find_best_moves(available_moves))
+        self.assertEqual(expected_sorted_moves, x.maximize_moves(available_moves))
         available_moves = [x.Move(row=2, col=0, score=-2)]
-        expected_best_moves = x.BestMoves(
+        expected_sorted_moves = x.SortedMoves(
             score=-2,
             moves=[x.Move(row=2, col=0, score=-2)]
         )
-        self.assertEqual(expected_best_moves, x.find_best_moves(available_moves))
+        self.assertEqual(expected_sorted_moves, x.maximize_moves(available_moves))
 
-    def test_select_best_move(self):
+    def test_minimize_moves(self):
+        available_moves = [
+            x.Move(row=0, col=1, score=-16),
+            x.Move(row=1, col=2, score=6),
+            x.Move(row=2, col=1, score=-1),
+            x.Move(row=2, col=2, score=1),
+            x.Move(row=2, col=2, score=6),
+        ]
+        expected_sorted_moves = x.SortedMoves(
+            score=-16,
+            moves=[
+                x.Move(row=0, col=1, score=-16),
+            ]
+        )
+        self.assertEqual(expected_sorted_moves, x.minimize_moves(available_moves))
+        available_moves = [x.Move(row=2, col=0, score=-2)]
+        expected_sorted_moves = x.SortedMoves(
+            score=-2,
+            moves=[x.Move(row=2, col=0, score=-2)]
+        )
+        self.assertEqual(expected_sorted_moves, x.minimize_moves(available_moves))
+
+    def test_select_sorted_move(self):
         seed = 0
-        best_moves = x.BestMoves(
+        sorted_moves = x.SortedMoves(
             score=6,
             moves=[
                 x.Move(row=1, col=2, score=6),
@@ -80,64 +96,123 @@ class TestMe(unittest.TestCase):
             ],
         )
         expect = x.Move(row=2, col=2, score=6)
-        self.assertEqual(expect, x.select_best_move(best_moves, seed))
-        best_moves = x.BestMoves(
+        self.assertEqual(expect, x.select_sorted_move(sorted_moves, seed))
+        sorted_moves = x.SortedMoves(
             score=-2,
             moves=[
                 x.Move(row=2, col=0, score=-2),
             ],
         )
         expect = x.Move(row=2, col=0, score=-2)
-        self.assertEqual(expect, x.select_best_move(best_moves, seed))
+        self.assertEqual(expect, x.select_sorted_move(sorted_moves, seed))
 
     def test_rank_available_moves(self):
-        test_board = [
-            ['x', 'o', 'x'],
-            ['o', 'x', 'o'],
-            ['o', 'x', None],
-        ]
-        self.assertEqual([x.Move(row=2, col=2, score=0)], x.rank_available_moves(test_board),
-                         "No win scenario should return None")
-        test_board = [
-            ['o', 'x', 'o'],
-            ['x', None, 'x'],
-            ['o', 'x', 'o'],
-        ]
-        self.assertEqual([x.Move(row=1, col=1, score=-10)], x.rank_available_moves(test_board))
-        test_board = [
-            [None, 'x', 'x'],
-            ['x', None, 'x'],
-            ['x', 'x', None],
-        ]
-        self.assertEqual([x.Move(row=0, col=0, score=10), x.Move(row=1, col=1, score=10), x.Move(row=2, col=2, score=10)], x.rank_available_moves(test_board, 'o'))
-        test_board = [
+        # 'x' should play on 1, 1 even though 'o' is in winning pos.
+        board = [
             ['o', None, 'x'],
             ['x', None, 'x'],
             [None, 'o', 'o'],
         ]
         expect = [
-            x.Move(row=0, col=1, score=9),
-            x.Move(row=1, col=1, score=-10),
-            x.Move(row=2, col=0, score=-2),
-        ]
-        self.assertEqual(expect, x.rank_available_moves(test_board))
-        test_board = [
-            ['o', None, 'x'],
-            ['x', None, 'x'],
-            [None, 'o', 'o'],
-        ]
-        expect = [
-            x.Move(row=0, col=1, score=-2),
+            x.Move(row=0, col=1, score=-11),
             x.Move(row=1, col=1, score=10),
-            x.Move(row=2, col=0, score=8),
+            x.Move(row=2, col=0, score=7),
         ]
-        self.assertEqual(expect, x.rank_available_moves(test_board, 'o'))
-        test_board = [
-            ['x', None, None],
-            [None, None, None],
-            [None, None, None],
+        self.assertEqual(expect, x.rank_available_moves(board, 'o'))
+
+        # 'o' should play on 1, 1 to prevent 'x from winning
+        board = [
+            ['o', None, 'x'],
+            ['x', None, 'x'],
+            ['x', 'o', 'o'],
         ]
-        # self.assertEqual([], x.rank_available_moves(test_board))
+        expect = [x.Move(row=0, col=1, score=-11), x.Move(row=1, col=1, score=10)]
+        self.assertEqual(expect, x.rank_available_moves(board))
+
+        # 'x' should play on 0, 1 to tie game
+        board = [
+            ['o', None, 'x'],
+            ['x', 'o', 'x'],
+            ['x', 'o', 'o'],
+        ]
+        expect = [x.Move(row=0, col=1, score=-10)]
+        self.assertEqual(expect, x.rank_available_moves(board, 'o'))
+
+        # No moves available should return empty array
+        board = [
+            ['o', 'x', 'x'],
+            ['x', 'o', 'x'],
+            ['x', 'o', 'o'],
+        ]
+        expect = []
+        self.assertEqual(expect, x.rank_available_moves(board))
+
+    def test_select_next_move(self):
+        # 'x' should play on 1, 1 even though 'o' is in winning pos.
+        board = [
+            ['o', None, 'x'],
+            ['x', None, 'x'],
+            [None, 'o', 'o'],
+        ]
+        human_player = 'o'
+        seed = 0
+        expect = x.Move(row=1, col=1, score=10)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
+
+        # 'o' should play on 1, 1 to prevent 'x from winning
+        board = [
+            ['o', None, 'x'],
+            ['x', None, 'x'],
+            ['x', 'o', 'o'],
+        ]
+        human_player = 'x'
+        seed = 0
+        expect = x.Move(row=1, col=1, score=10)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
+
+        # 'x' should play on 0, 1 to tie game
+        board = [
+            ['o', None, 'x'],
+            ['x', 'o', 'x'],
+            ['x', 'o', 'o'],
+        ]
+        human_player = 'o'
+        seed = 0
+        expect = x.Move(row=0, col=1, score=-10)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
+
+        # No moves available should return empty array
+        board = [
+            ['o', 'x', 'x'],
+            ['x', 'o', 'x'],
+            ['x', 'o', 'o'],
+        ]
+        expect = None
+        self.assertEqual(expect, x.select_next_move(board))
+
+        # Should pick corner
+        board = [[None, None, None], [None, None, None], [None, None, None]]
+        human_player = 'o'
+        seed = 0
+        expect = x.Move(row=2, col=2, score=0)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
+        board = [[None, None, None], [None, 'x', None], [None, None, None]]
+        human_player = 'x'
+        seed = 0
+        expect = x.Move(row=2, col=2, score=0)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
+        board = [[None, 'x', None], [None, None, None], [None, None, None]]
+        human_player = 'x'
+        seed = 0
+        expect = x.Move(row=2, col=2, score=0)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
+
+        # Should pick center
+        board = [['x', None, None], [None, None, None], [None, None, None]]
+        human_player = 'x'
+        seed = 0
+        expect = x.Move(row=1, col=1, score=0)
+        self.assertEqual(expect, x.select_next_move(board, human_player, seed))
 
 
 if __name__ == '__main__':
--- x.py
+++ x.py
@@ -4,10 +4,10 @@ from copy import deepcopy
 from collections import namedtuple
 
 Move = namedtuple('Move', ['row', 'col', 'score'])
-BestMoves = namedtuple('BestMoves', ['score', 'moves'])
+SortedMoves = namedtuple('SortedMoves', ['score', 'moves'])
 
 
-def select_next_move(board, human_player='x'):
+def select_next_move(board, human_player='x', seed=None):
     """
     Runs recursive function to rank available moves then returns the best move
 
@@ -15,9 +15,45 @@ def select_next_move(board, human_player='x'):
     :param human_player: token used by the human player
     :return: Move
     """
+    if is_first_two_moves(board):
+        return select_first_two_moves(board, seed)
+
+    # if all available moves are losing moves, choose most negatively scored move
     ranked_available_moves = rank_available_moves(board, human_player)
-    best_moves = find_best_moves(ranked_available_moves)
-    return select_best_move(best_moves)
+    if are_all_losing_moves(ranked_available_moves):
+        sorted_moves = minimize_moves(ranked_available_moves)
+    else:
+        sorted_moves = maximize_moves(ranked_available_moves)
+    return select_sorted_move(sorted_moves, seed)
+
+
+def is_first_two_moves(board):
+    joined_board = board[0] + board[1] + board[2]
+    return joined_board.count(None) > 7
+
+
+def select_first_two_moves(board, seed=None):
+    # if all corners are free, pick a corner else pick center
+    corners = [
+        board[0][0],
+        board[0][2],
+        board[2][0],
+        board[2][2],
+    ]
+    if corners.count(None) == 4:
+        sorted_moves = SortedMoves(
+            score=0,
+            moves=[
+                Move(row=0, col=0, score=0),
+                Move(row=0, col=2, score=0),
+                Move(row=2, col=0, score=0),
+                Move(row=2, col=2, score=0),
+            ],
+        )
+    else:
+        sorted_moves = SortedMoves(score=0, moves=[Move(row=1, col=1, score=0)])
+
+    return select_sorted_move(sorted_moves, seed)
 
 
 def rank_available_moves(board, human_player='x', recursive_level=0):
@@ -29,64 +65,93 @@ def rank_available_moves(board, human_player='x', recursive_level=0):
     :param recursive_level: depth of recursion
     :return: array of Move
     """
-    # find which player goes next in the game
-    next_player = who_goes_next2(board)
-    if not next_player:
-        return []
+    available_moves = []
+
+    # find who's turn it is
+    current_turn = get_current_turn(board)
+    if not current_turn:
+        return available_moves
 
     # go through each empty space and find available moves
-    available_moves = []
     for row_i in range(3):
         for col_i in range(3):
             new_board = board.copy()
             if not new_board[row_i][col_i]:
                 new_board = deepcopy(board)
-                new_board[row_i][col_i] = next_player
-                winner = find_winner(new_board)
+                new_board[row_i][col_i] = current_turn
+                possible_win_groups = collate_possible_win_groups(new_board)
+                winner = find_winner(possible_win_groups)
                 if winner == human_player:
                     available_moves.append(Move(row=row_i, col=col_i, score=-10 - recursive_level))
                 elif winner != human_player and winner is not None:
                     available_moves.append(Move(row=row_i, col=col_i, score=10 - recursive_level))
                 else:
-                    available_moves2 = rank_available_moves(new_board, human_player, recursive_level + 1)
-                    if not available_moves2:
+                    recursed_moves = rank_available_moves(new_board, human_player, recursive_level + 1)
+                    if not recursed_moves:
                         available_moves.append(Move(row=row_i, col=col_i, score=0 - recursive_level))
                     else:
-                        best_move = find_best_moves(available_moves2)
-                        available_moves.append(Move(row=row_i, col=col_i, score=best_move.score))
+                        if current_turn == human_player:
+                            sorted_moves = minimize_moves(recursed_moves)
+                        else:
+                            sorted_moves = maximize_moves(recursed_moves)
+                        available_moves.append(Move(row=row_i, col=col_i, score=sorted_moves.score - recursive_level))
 
     return available_moves
 
 
-def find_best_moves(available_moves):
+def maximize_moves(available_moves):
+    def max_min_func(sorted_moves, move): return sorted_moves.score < move.score
+    default_score = -sys.maxsize
+    return max_min_moves(available_moves, max_min_func, default_score)
+
+
+def minimize_moves(available_moves):
+    def max_min_func(sorted_moves, move): return sorted_moves.score > move.score
+    default_score = sys.maxsize
+    return max_min_moves(available_moves, max_min_func, default_score)
+
+
+def max_min_moves(available_moves, max_min_func, default_score):
     """
     Sorts available moves and returns array of equally ranked moves
 
     :param available_moves: array of Moves
-    :return: BestMoves
+    :param max_min_func:
+    :param default_score:
+    :return: SortedMoves
     """
-    best_moves = BestMoves(score=-sys.maxsize, moves=[])
+    sorted_moves = SortedMoves(score=default_score, moves=[])
     for move in available_moves:
-        if best_moves.score < move.score:
-            best_moves = BestMoves(score=move.score, moves=[move])
-        elif best_moves.score == move.score:
-            best_moves.moves.append(move)
-    return best_moves
+        if max_min_func(sorted_moves, move):
+            sorted_moves = SortedMoves(score=move.score, moves=[move])
+        elif sorted_moves.score == move.score:
+            sorted_moves.moves.append(move)
+    return sorted_moves
 
 
-def select_best_move(best_moves, random_seed=None):
+def select_sorted_move(sorted_moves, random_seed=None):
     """
     When given an array of equally ranked Moves, choose one.
 
-    :param best_moves: array of Moves
+    :param sorted_moves: array of Moves
     :param random_seed: optional seed for random method
     :return: Move
     """
+    if not sorted_moves.moves:
+        return None
+
     random.seed(random_seed)
-    return random.choice(best_moves.moves)
+    return random.choice(sorted_moves.moves)
 
 
-def who_goes_next2(board):
+def are_all_losing_moves(available_moves):
+    for move in available_moves:
+        if move.score > -10:
+            return False
+    return True
+
+
+def get_current_turn(board):
     """
     Choose next piece to go based on current piece
 
@@ -100,18 +165,6 @@ def who_goes_next2(board):
     if num_none == 0:
         return None
     return 'o' if num_none % 2 == 0 else 'x'
-
-
-def who_goes_next(piece):
-    """
-    Choose next piece to go based on current piece
-
-    :param piece: None, 'x' or 'o'
-    :return: 'x' or 'o'
-    """
-    if piece != 'x' and piece != 'o':
-        return 'x'
-    return 'x' if piece == 'o' else 'o'
 
 
 def collate_possible_win_groups(board):
@@ -146,3 +199,8 @@ def find_winner(possible_win_groups):
         if possible_win.count('o') == 3:
             return 'o'
     return None
+
+
+if __name__ == '__main__':
+    pass
+
